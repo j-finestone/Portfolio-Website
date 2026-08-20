1878550b03f9b542dc230c4f7b3e574103b05828
j-finestone <100159764+j-finestone@users.noreply.github.com>
2026-08-20 15:23:19 -0400

    Make some paths relative

--- code/js/components/components.js
+++ code/js/components/components.js
@@ -1,10 +1,10 @@
-fetch("code/js/components/header.html")
+fetch("./code/js/components/header.html")
     .then(response=>response.text())
     .then(data => {
         document.getElementById("header").innerHTML = data;
     });
 
-fetch("code/js/components/footer.html")
+fetch("./code/js/components/footer.html")
     .then(response=>response.text())
     .then(data => {
         document.getElementById("footer").innerHTML = data;
