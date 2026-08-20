e5f89db0244b90f43ab625ec576eed4d42b960dd
j-finestone <100159764+j-finestone@users.noreply.github.com>
2026-08-20 15:09:13 -0400

    Minor Styling Changes

--- code/js/projects/cardLoader.js
+++ code/js/projects/cardLoader.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <img class="card-thumbnail" src="` + cards[i].thumbnail+ ` "alt=". `+ cards[i].header + ` project thumbnail" width="100%"> 
         </section>   
         <section class="card-description">
-            <h1>`+cards[i].header+`</h1>
+            <h3>`+cards[i].header+`</h3>
             <p>`+cards[i].description+`</p>
         </section`;
 
